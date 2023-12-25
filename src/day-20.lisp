638677bc7e561ae48c63f8a4b23c3f6211c14078
Christoph Göttschkes <christoph.goettschkes@posteo.net>
2023-12-25 14:49:21 +0100

    Day 20 - part 2.

--- src/day-20.lisp
+++ src/day-20.lisp
@@ -1,10 +1,11 @@
 (defpackage #:aoc-2023/day-20
   (:use #:cl)
-  (:export #:pulse-propagation-1))
+  (:export #:pulse-propagation-1
+           #:pulse-propagation-2))
 
 (in-package #:aoc-2023/day-20)
 
-(declaim (optimize (speed 3) (debug 0) (safety 0)))
+(declaim (optimize (speed 0) (debug 3) (safety 3)))
 (setf (documentation *package* t) "Day 20: Pulse Propagation")
 
 (defstruct module
@@ -12,10 +13,8 @@
   (name)
   (handler)
   (state)
+  (out-was-high-once)
   (connected))
-
-(defmacro high-p (pulse) `(< 0 ,pulse))
-(defmacro low-p (pulse) `(> 0 ,pulse))
 
 (defun parse-out (str name-to-index)
   (loop :for pos = 0 :then (1+ (or (position #\, str :start pos) (length str)))
@@ -65,8 +64,17 @@
                                  (setf (aref result (module-idx rx-module)) rx-module)))
                              (values start result))))))
 
+(defun find-inputs-for (modules name)
+  (let ((target-num (loop :for i :upfrom 0 :below (length modules)
+                          :when (string= (module-name (aref modules i)) name)
+                            :return i)))
+    (loop :for i :upfrom 0 :below (length modules)
+          :when (find-if (lambda (x) (= x target-num)) (module-connected (aref modules i)))
+            :collect i)))
+
 (defun modules-initialize-state (modules)
   (loop :for m1 :across modules
+        :do (setf (module-out-was-high-once m1) nil)
         :when (or (eq (module-handler m1) #'broadcast-pulse-handler)
                   (eq (module-handler m1) #'flip-flop-pulse-handler))
           :do (setf (module-state m1) -1)
@@ -78,16 +86,18 @@
                           (make-array (length modules) :initial-element -1)))))
 
 (defun broadcast-pulse-handler (module input input-from)
-  (declare (ignore module)
-           (ignore input-from))
+  (declare (ignore input-from))
+  (setf (module-out-was-high-once module) (or (module-out-was-high-once module) (= 1 input)))
   input)
 
 (defun flip-flop-pulse-handler (module input input-from)
   (declare (ignore input-from))
-  (if (< 0 input)
-      0
-      (progn (setf (module-state module) (- 0 (module-state module)))
-             (module-state module))))
+  (let ((out (if (< 0 input)
+                 0
+                 (progn (setf (module-state module) (- 0 (module-state module)))
+                        (module-state module)))))
+    (setf (module-out-was-high-once module) (or (module-out-was-high-once module) (= 1 out)))
+    out))
 
 (defun conjunction-pulse-handler (module input input-from)
   (cond ((and (= 1 input) (= -1 (aref (nth 2 (module-state module)) (module-idx input-from))))
@@ -96,10 +106,12 @@
         ((and (= -1 input) (= 1 (aref (nth 2 (module-state module)) (module-idx input-from))))
          (decf (cadr (module-state module)))
          (setf (aref (nth 2 (module-state module)) (module-idx input-from)) input)))
-  (if (= (nth 0 (module-state module))
-         (nth 1 (module-state module)))
-      -1
-      +1))
+  (let ((out (if (= (nth 0 (module-state module))
+                    (nth 1 (module-state module)))
+                 -1
+                 +1)))
+    (setf (module-out-was-high-once module) (or (module-out-was-high-once module) (= 1 out)))
+    out))
 
 (defun rx-pulse-handler (module input input-from)
   (declare (ignore input-from))
@@ -123,6 +135,20 @@
                         :do (setq queue (append queue (list (list (aref modules m2) out module))))))))
     (values low-count high-count)))
 
+(defun count-button-presses-until-high (modules start target)
+  (loop :for count :upfrom 0
+        :when (module-out-was-high-once target)
+          :return count
+        :do (press-button modules start)))
+
+(defun find-inputs-for (modules name)
+  (let ((target-num (loop :for i :upfrom 0 :below (length modules)
+                          :when (string= (module-name (aref modules i)) name)
+                            :return i)))
+    (loop :for module :across modules
+          :when (find-if (lambda (x) (= x target-num)) (module-connected module))
+            :collect module)))
+
 (defun pulse-propagation-1 (stream)
   (multiple-value-bind (start modules)
       (parse stream)
@@ -134,3 +160,16 @@
           (incf lc low-count)
           (incf hc high-count)))
       (* lc hc))))
+
+(defun pulse-propagation-2 (stream)
+  (multiple-value-bind (start modules)
+      (parse stream)
+    (let* ((inputs-of-rx (find-inputs-for modules "rx"))
+           (inputs-of-rx-input (find-inputs-for modules (module-name (car inputs-of-rx)))))
+      (if (and (= 1 (length inputs-of-rx))
+               (every (lambda (mod) (eq (module-handler mod) #'conjunction-pulse-handler)) inputs-of-rx-input))
+          (apply #'lcm
+                 (loop :for target :in inputs-of-rx-input
+                       :do (modules-initialize-state modules)
+                       :collect (count-button-presses-until-high modules start target)))
+          0))))
